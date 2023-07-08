c2d21d6937606f1548dc3120032786376df784eb
dinesh.ithub <dinesh.ithub@gmail.com>
2023-07-09 00:41:05 +0100

    DC

--- 3_DataTypes.py
+++ 3_DataTypes.py
@@ -9,3 +9,9 @@ print("The type of b", type(b))
 c = 1+3j  
 print("The type of c", type(c))  
 print(" c is a complex number", isinstance(1+3j,complex))
+
+# Boolean
+# Python program to check the boolean type  
+print(type(True))  
+print(type(False))  
+print(false)  
---
+++ 8_SET.py
@@ -0,0 +1,17 @@
+# Creating Empty set  
+set1 = set()  
+  
+set2 = {'James', 2, 3,'Python'}  
+  
+#Printing Set value  
+print(set2)  
+  
+# Adding unique & Dupliacte element to the set  
+  
+set2.add(10)
+set2.add(2)
+print(set2)  
+  
+#Removing element from the set  
+set2.remove(2)  
+print(set2)
